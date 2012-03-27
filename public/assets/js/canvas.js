c56f4da71f7c3c4b7b551e7d8219a413217840d9
Robert Sargant <robert@sargant.com>
2012-03-27 20:15:32 +0100

    Added simple pRNG and limit calligraphy threshold to stop big ugly swathes

--- public/assets/js/canvas.js
+++ public/assets/js/canvas.js
@@ -1,6 +1,45 @@
 
 // Some namespaced helper functions that do not require the page to be loaded
 var canvasUtils = new function () {
+
+	// Use MurmurHash32 as a source of low-quality, reproducible pseudorandom numbers
+	//
+	// Copyright (c) 2011 Gary Court
+	//
+	// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+	// software and associated documentation files (the "Software"), to deal in the Software
+	// without restriction, including without limitation the rights to use, copy, modify, merge,
+	// publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+	// persons to whom the Software is furnished to do so, subject to the following conditions:
+	//
+	// The above copyright notice and this permission notice shall be included in all copies
+	// or substantial portions of the Software.
+	var murmurhash2_32_gc = function(str, seed) {
+		var l = str.length, h = seed ^ l, i = 0, k
+		while (l >= 4) {
+			k = ((str.charCodeAt(i)&0xff))|((str.charCodeAt(++i)&0xff)<<8)|((str.charCodeAt(++i)&0xff)<<16)|((str.charCodeAt(++i)&0xff)<<24)
+			k = (((k&0xffff)*0x5bd1e995)+((((k>>>16)*0x5bd1e995)&0xffff)<<16))
+			k ^= k>>>24
+			k = (((k&0xffff)*0x5bd1e995)+((((k>>>16)*0x5bd1e995)&0xffff)<<16))
+			h = (((h&0xffff)*0x5bd1e995)+((((h>>>16)*0x5bd1e995)&0xffff)<<16))^k
+			l -= 4
+			++i
+		}
+		switch (l) {
+			case 3: h ^= (str.charCodeAt(i+2)&0xff)<<16
+			case 2: h ^= (str.charCodeAt(i+1)&0xff)<< 8
+			case 1: h ^= (str.charCodeAt(i)&0xff)
+				h = (((h&0xffff)*0x5bd1e995)+((((h>>>16)*0x5bd1e995)&0xffff)<<16))
+		}
+		h ^= h>>>13
+		h = (((h&0xffff)*0x5bd1e995)+((((h>>>16)*0x5bd1e995)&0xffff)<<16))
+		h ^= h>>>15
+		return h>>>0
+	}
+	
+	this.poorRandom = function (string) {
+		return parseInt(murmurhash2_32_gc(string).toString().substr(-6)) / 1000000
+	}
 	
 	this.isCanvasSupported = function () {
 		var e = document.createElement('canvas')
@@ -99,8 +138,13 @@ var canvasUtils = new function () {
 				
 				for(var i = 1; i <= maxHistory; i++) {
 					if(history[history.length-i] == "break") break
-					ctx.moveTo(history[history.length-i][0], history[history.length-i][1])
-					ctx.lineTo(coords[2], coords[3])
+					var a = history[history.length-i][0]
+					var b = history[history.length-i][1]
+					// Limit distance to prevent vast inky swathes
+					if(((coords[2]-a) * (coords[2]-a) + (coords[2]-a) * (coords[2]-a)) < 10000) {
+						ctx.moveTo(history[history.length-i][0], history[history.length-i][1])
+						ctx.lineTo(coords[2], coords[3])
+					}
 				}
 				
 				addPaintLog(brush.type, [coords[0], coords[1]])
